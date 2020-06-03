cb9ee1acf62fd15d749aca6973dfa1a13f0ae2fb
overgara <overgara@DESKTOP-MT9USRG>
2020-06-03 01:24:47 -0400

    se quitan warnings

--- src/main/java/com/poc/vehicle/controller/VehicleSettersController.java
+++ src/main/java/com/poc/vehicle/controller/VehicleSettersController.java
@@ -1,6 +1,5 @@
 package com.poc.vehicle.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
